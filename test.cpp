8b0039ee1cc4ac5b4ae0cdc1ad46a32efa3339ac
BrianDeanO <brianrobertdean@gmail.com>
2023-02-08 19:21:48 -0800

    Initializing the Space Invaders github repository with a test.cpp file. 2/8/23

---
+++ test.cpp
@@ -0,0 +1,15 @@
+/*
+    Brian Dean
+    Space Invaders Project
+    CPTS 322
+    8 February 2023
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Setting Up The Space Invaders Github!\n";
+
+    return 0;
+}
